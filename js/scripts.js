8bb723bd4d65b459fed2ce770ff903ba347d2ad5
derrrick william <williamderrick100@gmail.com>
2018-12-10 18:13:57 +0300

    Corrected made changes on the file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,43 @@
 $(document).ready(function () {
- $(".container").hide();
-}
+$(".question").hide();
+ $("#q1").slideDown();
+
+$("#next2").click(function(){
+  $("#q2").slideDown();
+});
+$("#next3").click(function(){
+  $("#q3").slideDown();
+});
+$("#next4").click(function(){
+  $("#q4").slideDown();
+});
+$("#next5").click(function(){
+  $("#q5").slideDown();
+});
+$("#next6").click(function(){
+  $("#q6").slideDown();
+});
+$("#next7").click(function(){
+  $("#q7").slideDown();
+});
+$("#next8").click(function(){
+  $("#q8").slideDown();
+});
+$("#next9").click(function(){
+  $("#q9").slideDown();
+});
+$("#next10").click(function(){
+  $("#q10").slideDown();
+});
+});
+
+
+//
+// $(document).ready(function() {
+// $("button#nextx").click(function() {
+// $("#q1").hide();
+// $("#q2").show();
+//
+//
+// });
+// });
