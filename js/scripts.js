dd029591f9317a360f9608d9526c80e8ab99d2cb
derrrick william <williamderrick100@gmail.com>
2018-12-11 07:20:42 +0300

    Added Finish Button

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 var blank = false;
 $("input:radio").each(function() {
-  var val = $('input:radio[name=' + this.name + ']:checked').val();
+  var val = $('input:radio[name="" + this.name + ']:checked').val();
   if (val === undefined) {
     blank = true;
     return false;
@@ -13,32 +13,7 @@ var answer = $(input: radio[answer] val();)
 
 
 
-$(document).ready(function() {
-  $(".question").hide();
-  $("#q1").slideDown();
 
-  $("#next2").click(function() {
-    $("#q3").slideDown();
-  });
-  $("#next3").click(function() {
-    $("#q4").slideDown();
-  });
-  $("#next").click(function() {
-    $("#q4").slideDown();
-  });
-  $("#next5").click(function() {
-    $("#q5").slideDown();
 
-  });
 });
-
-
-//
-// $(document).ready(function() {
-// $("button#nextx").click(function() {
-// $("#q1").hide();
-// $("#q2").show();
-//
-//
-// });
-// });
+});
