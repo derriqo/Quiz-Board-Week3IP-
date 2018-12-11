6287dc62a6b33c4137b1ed5cb5d8dd6675538561
derrrick william <williamderrick100@gmail.com>
2018-12-11 11:23:09 +0300

    Finalised on the project.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,28 @@
+// Bussiness logic:\
+var sum = 0;
+
+function totalMarks() {
+  var questions = ["answer", "answer1", "answer2", "answer3", "answer4"];
+  questions.forEach(function(question) {
+    var userChoice = $("input:radio[name=" + question + "]:checked").val();
+    if (userChoice == "20") {
+      sum += 20;
+    }
+  });
+  return sum;
+}
+
 $(document).ready(function() {
-  $("#finish").click(function (){
+
+
+  $("#finish").click(function() {
+    var total = totalMarks();
     if (!$("input[type='radio']:checked").val()) {
       alert('Please select an answer');
-      return false;
+    } else {
+      $(".marks").text(total + "/100");
 
     }
-  })
+
+  });
 });
